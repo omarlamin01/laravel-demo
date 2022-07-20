6d9204fdec445bf62a99d6c65bb782ef71186c29
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2022-07-20 18:36:26 +0000

    add alpineJS link

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -13,6 +13,7 @@
         referrerpolicy="no-referrer"
     />
     <script src="https://cdn.tailwindcss.com"></script>
+    <script src="//unpkg.com/alpinejs" defer></script>
     <script>
         tailwind.config = {
             theme: {
